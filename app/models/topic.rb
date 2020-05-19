7b37d995c852602b60287a29b936c3a4da97f755
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-05-18 22:52:55 -0400

    add slugifiable module and instance method slug for User and Topic models

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ActiveRecord::Base
 	has_many :lists
 	has_many :users, through: :lists
+
+	extend Slugifiable::ClassMethods
+
+	def slug
+		self.name.gsub(" ", "-").downcase
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
 	has_many :lists
 	has_many :topics, through: :lists
+
+	extend Slugifiable::ClassMethods
+
+	def slug
+		self.username.gsub(" ", "-").downcase
+	end
 end
