9465dcf238cc174a934fb95ffa9915034a1c1a90
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-05-18 22:27:55 -0400

    add database cleaner gem. add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
-# shaq = User.create(name: "Shaquille Galimba", username: "Shaqriver", password: "Secret")
-# emily = User.create
+shaq = User.create(name: "Shaquille Galimba", username: "Shaqriver", password: "Secret")
+emily = User.create(name: "Emily Axford", username: "Eaxford", password: "Gilear")
+
+shows = Topic.create(name: "Shows")
+npcs = Topic.create(name: "Fantasy High NPCs")
+
+List.create(contents: ["Dimension20", "Community", "The Office", "Breaking bad", "Friends"], user_id: shaq.id, topic_id: shows.id)
+
+List.create(contents: ["Gilear", "Gorthalax", "Ayda Aguefort", "Sandralyn", "Jawbone"], user_id: emily.id, topic_id: npcs.id)
