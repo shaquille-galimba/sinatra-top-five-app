0f55a2cb80b2cf687f6b9f5f569b423b5bbcdab7
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-05-19 01:05:09 -0400

    update arrayed_contents method from List model

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,6 @@ class List < ActiveRecord::Base
 	belongs_to :topic
 
 	def arrayed_contents
-		self.contents.gsub("[", "").gsub("]", "").split(",")
+		self.contents.scan(/\"(.*?)\"/).flatten
 	end
 end
