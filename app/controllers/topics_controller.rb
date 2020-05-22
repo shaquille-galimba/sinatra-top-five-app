a4bd1d3d50cde74d8065bce354691f02b1d7b4b3
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-05-22 00:41:04 -0400

    add feature to create a new list for an already existing topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,17 +2,22 @@ class TopicsController < ApplicationController
 
 	# => read
 	get '/topics' do
-		if logged_in?
-			@topics = Topic.order(:name)
-			erb :'topics/index'
-		else
-			redirect '/'
-		end
+		@topics = Topic.order(:name)
+		erb :'topics/index'
 	end
 
 	get '/topics/:slug' do
 		@topic = Topic.find_by_slug(params[:slug])
 		erb :'topics/show'
+	end
+
+	post '/topics/:slug/new' do
+		if logged_in?
+			@topic = Topic.find_by_slug(params[:slug])
+			erb :'/lists/new'
+		else
+			redirect '/'
+		end
 	end
 
 end
